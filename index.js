b2f7c0b44e244524d4298df390b93bb81211edf0
partha <spartha343@gmail.com>
2024-11-20 13:14:01 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT | 5000;
+
+const uri = process.env.DATABASE_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+
+async function run() {
+  try {
+    const categoryCollection = client.db("pc-builder").collection("categories");
+    const productCollection = client.db("pc-builder").collection("products");
+
+    app.get("/categories", async (req, res) => {
+      const categories = await categoryCollection.find({}).toArray();
+      res.send(categories);
+    });
+
+    app.get("/products", async (req, res) => {
+      const products = await productCollection.find({}).toArray();
+      res.send(products);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const { id } = req.params;
+      const product = await productCollection.findOne({
+        _id: id
+      });
+      res.send(product);
+    });
+
+    app.get("/products-by-category-name/:categoryName", async (req, res) => {
+      const categoryName = req.params.categoryName;
+      const products = await productCollection
+        .find({ category: categoryName })
+        .toArray();
+      res.send(products);
+    });
+
+    app.get("/featured-products", async (req, res) => {
+      const products = await productCollection
+        .aggregate([{ $sample: { size: 6 } }])
+        .toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello from pc builder server!");
+});
