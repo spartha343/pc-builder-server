bf96810a01e6815feeed2371962aab62b90a75c8
partha <spartha343@gmail.com>
2024-11-20 13:49:02 +0600

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT | 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const uri = process.env.DATABASE_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
